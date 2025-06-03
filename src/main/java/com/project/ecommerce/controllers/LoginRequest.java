f9a0a3f74f28761d67385d8a4d990fc4cd8069dc
nihonor <niyitanganihonor@gmail.com>
2025-06-03 22:16:17 +0200

    creating login request

---
+++ src/main/java/com/project/ecommerce/controllers/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.project.ecommerce.controllers;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @Email
+    @NotBlank(message = "Email must not be blank")
+    private String email;
+    @NotBlank(message = "password must not be blank")
+    private String password;
+}
