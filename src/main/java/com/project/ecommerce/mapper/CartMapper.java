a50b3a01beecf815be8a136e541fb8a7b32c5886
nihonor <niyitanganihonor@gmail.com>
2025-06-03 22:14:48 +0200

    creating cart mapper

--- src/main/java/com/project/ecommerce/mapper/CartMapper.java
+++ src/main/java/com/project/ecommerce/mapper/CartMapper.java
@@ -1,10 +1,18 @@
 package com.project.ecommerce.mapper;
 
 import com.project.ecommerce.dtos.CartDto;
+import com.project.ecommerce.dtos.CartItemDto;
 import com.project.ecommerce.entities.Cart;
+import com.project.ecommerce.entities.CartItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
+    @Mapping(target = "items",source = "items")
+    @Mapping(target = "totalPrice",expression = "java(cart.getTotalPrice())")
     CartDto toDto(Cart cart);
+    @Mapping(target = "totalPrice",expression = "java(cartItem.getTotalPrice())")
+    @Mapping(target = "product.id", source = "product.id")
+    CartItemDto toItemDto(CartItem cartItem);
 }
