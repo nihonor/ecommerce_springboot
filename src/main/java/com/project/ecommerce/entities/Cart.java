187dcccd6fb856fcb21df3c5223c1b755108169d
nihonor <niyitanganihonor@gmail.com>
2025-06-03 22:09:41 +0200

    adding the get total price implementation in cart entity

--- src/main/java/com/project/ecommerce/entities/Cart.java
+++ src/main/java/com/project/ecommerce/entities/Cart.java
@@ -1,11 +1,10 @@
 package com.project.ecommerce.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -25,7 +24,52 @@ public class Cart {
     @Column(name = "date_created",insertable = false, updatable = false)
     private LocalDate dateCreated;
 
-    @OneToMany(mappedBy = "cart")
-    private Set<CartItem> cartItems = new LinkedHashSet<>();
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.MERGE, fetch = FetchType.EAGER,orphanRemoval = true)
+    private Set<CartItem> items = new LinkedHashSet<>();
+
+    public BigDecimal getTotalPrice(){
+        BigDecimal total = BigDecimal.ZERO;
+        for(CartItem item : items){
+            total = total.add(item.getTotalPrice());
+        }
+        return total;
+    }
+
+    public CartItem getItem(Long productId){
+        return items.stream()
+                .filter(item->item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElse(null);
+
+    }
+
+    public CartItem addItem(Product product){
+        var cartItem=getItem(product.getId());
+        if(cartItem!=null){
+            cartItem.setQuantity(cartItem.getQuantity()+1);
+        }
+        else {
+            cartItem=new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(this);
+            items.add(cartItem);
+        }
+        return cartItem;
+    }
+
+    public void removeItem(Long productId){
+        var carItem=getItem(productId);
+        if(carItem!=null){
+            items.remove(carItem);
+        }
+        carItem.setCart(null);
+    }
+
+    public void clearCart(){
+        items.clear();
+    }
+
+
 
 }
