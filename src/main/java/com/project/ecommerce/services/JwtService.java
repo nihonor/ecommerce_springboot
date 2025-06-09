3267737423c3c0f6b0d31cdeb833a7aaa4b621be
nihonor <niyitanganihonor@gmail.com>
2025-06-09 21:04:27 +0200

    adding email and name as claims

--- src/main/java/com/project/ecommerce/services/JwtService.java
+++ src/main/java/com/project/ecommerce/services/JwtService.java
@@ -1,5 +1,6 @@
 package com.project.ecommerce.services;
 
+import com.project.ecommerce.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
@@ -13,10 +14,12 @@ import java.util.Date;
 public class JwtService {
     @Value("${spring.jwt.secret}")
     private String secret;
-    public String generateToken(String email) {
+    public String generateToken(User user) {
         final long tokenExpiration = 86400;
         return Jwts.builder()
-                .subject(email)
+                .subject(user.getId().toString())
+                .claim("email",user.getEmail())
+                .claim("name", user.getName())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
@@ -41,7 +44,7 @@ public class JwtService {
                 .getPayload();
     }
 
-    public String getUserEmailFromToken(String token) {
-        return getClaims(token).getSubject();
+    public Long getUserIdFromToken(String token) {
+        return Long.valueOf(getClaims(token).getSubject());
     }
 }
