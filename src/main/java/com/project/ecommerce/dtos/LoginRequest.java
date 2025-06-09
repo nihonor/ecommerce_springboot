a746f979ae8fb42913e17b08a41229bdbe17bb55
nihonor <niyitanganihonor@gmail.com>
2025-06-09 21:05:37 +0200

    changing LoginRequest folder

--- src/main/java/com/project/ecommerce/dtos/LoginRequest.java
+++ src/main/java/com/project/ecommerce/dtos/LoginRequest.java
@@ -1,7 +1,6 @@
-package com.project.ecommerce.controllers;
+package com.project.ecommerce.dtos;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
