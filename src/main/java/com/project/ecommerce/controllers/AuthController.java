5c9ef5864fed40c17963982bcb755f7484825dd8
nihonor <niyitanganihonor@gmail.com>
2025-06-09 21:03:14 +0200

    adding route to display the current user

--- src/main/java/com/project/ecommerce/controllers/AuthController.java
+++ src/main/java/com/project/ecommerce/controllers/AuthController.java
@@ -2,6 +2,9 @@ package com.project.ecommerce.controllers;
 
 
 import com.project.ecommerce.dtos.JwtResponse;
+import com.project.ecommerce.dtos.LoginRequest;
+import com.project.ecommerce.dtos.UserDto;
+import com.project.ecommerce.mapper.UserMapper;
 import com.project.ecommerce.repositories.UserRepository;
 import com.project.ecommerce.services.JwtService;
 import jakarta.validation.Valid;
@@ -11,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +26,7 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(
@@ -29,7 +34,8 @@ public class AuthController {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginRequest.getEmail(), loginRequest.getPassword()
         ));
-        var token=jwtService.generateToken(loginRequest.getEmail());
+        var user=userRepository.findByEmail(loginRequest.getEmail()).orElseThrow();
+        var token=jwtService.generateToken(user);
         return ResponseEntity.ok(new JwtResponse(token));
 
     }
@@ -45,4 +51,17 @@ public class AuthController {
         return jwtService.validateToken(token);
 
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        var authentication=SecurityContextHolder.getContext().getAuthentication();
+        var userId=(Long)authentication.getPrincipal();
+        var user=userRepository.findById(userId).orElse(null);
+        if(user==null){
+            return ResponseEntity.notFound().build();
+        }
+        var userDto=userMapper.toDto(user);
+        return ResponseEntity.ok(userDto);
+    }
+
 }
